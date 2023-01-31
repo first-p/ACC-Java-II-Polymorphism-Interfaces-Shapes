f7cd8954b319676c4fcc1ed9bc4913f21e64d560
first-p <98608650+first-p@users.noreply.github.com>
2023-01-31 16:09:24 -0600

    finished Square class

--- Square.java
+++ Square.java
@@ -1,3 +1,26 @@
 public class Square extends TwoDimensionalShape {
-    
+    private double edge;
+
+    //constructor
+    public Square(double edge){
+        if (edge <= 0)
+            throw new IllegalArgumentException(
+                "edge must be greater than 0");
+        setEdge(edge);
+    }
+
+    //set edge
+    public void setEdge(double edge){
+        this.edge = edge;
+    }
+    //get Edge
+    public double getEdge(){
+        return edge;
+    }
+
+    //Override getArea from Shape, returns area
+    @Override
+    public double getArea(){
+        return getEdge() * getEdge();
+    }
 }
